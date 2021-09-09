4b1958de2e87f125f5682ef445619d41fe6f982f
isakilikya <isaimadoglu@gmail.com>
2021-09-09 07:09:11 +0300

    products api yeniden konumlandirildi.

--- manavsepeti_django/base/views.py
+++ manavsepeti_django/base/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def get_routes(request):
+    routes = [
+        '/api/v1/products',
+        '/api/v1/products/<id>',
+        '/api/v1/products/create',
+        '/api/v1/products/delete/<id>',
+        '/api/v1/products/<update>/<id>',
+        '/api/v1/products/upload',
+        '/api/v1/products/<id>/reviews',
+        '/api/v1/products/top',
+    ]
+    return JsonResponse(routes, safe=False)
