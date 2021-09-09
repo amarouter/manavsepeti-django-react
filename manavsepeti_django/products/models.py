bb80db7c028a124768d4525c6f0a130f01439ef1
isakilikya <isaimadoglu@gmail.com>
2021-09-09 21:51:54 +0300

    ProductReview modeli eklendi.

--- manavsepeti_django/products/models.py
+++ manavsepeti_django/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -13,3 +14,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
     count_in_stock = models.IntegerField(blank=True, null=True, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductReview(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    comment = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
