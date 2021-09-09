8522479023e426228469f4d8a2d690d5f3d6ce90
isakilikya <isaimadoglu@gmail.com>
2021-09-09 17:03:30 +0300

    Product modeli eklendi.

--- manavsepeti_django/products/models.py
+++ manavsepeti_django/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    origin = models.CharField(max_length=100, blank=True, null=True)
+    # image =
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    count_in_stock = models.IntegerField(blank=True, null=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
