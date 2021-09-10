0c7a616be3928f80615b45be4aba93518e582088
isakilikya <isaimadoglu@gmail.com>
2021-09-10 06:45:53 +0300

    fix: Product modelin baska app'te import edilmesi.

--- manavsepeti_django/orders/models.py
+++ manavsepeti_django/orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ..products.models import Product
+# noinspection PyUnresolvedReferences
+from products.models import Product
 
 
 # Create your models here.
