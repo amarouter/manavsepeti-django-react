253c0366c2ea045260841a108a97742df3d21f98
isakilikya <isaimadoglu@gmail.com>
2021-09-12 03:54:17 +0300

    jwt ve register islemleri

--- manavsepeti_django/base/views.py
+++ manavsepeti_django/base/views.py
@@ -1,5 +1,15 @@
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import (
+    UserSerializer,
+    UserSerializerWithToken,
+    MyTokenObtainPairSerializer
+)
 
 
 # Create your views here.
@@ -17,3 +27,40 @@ def get_routes(request):
         '/api/v1/products/top',
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = 'User with this email already exists'
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
