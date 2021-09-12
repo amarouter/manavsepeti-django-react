c8b21e0aceb12a78862fc825f76a8e43da79f077
isakilikya <isaimadoglu@gmail.com>
2021-09-12 22:19:36 +0300

    products app'i icin ornek iki unit test

--- manavsepeti_django/products/tests.py
+++ manavsepeti_django/products/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+from .models import Product
+
 # Create your tests here.
+
+
+class ProductModelTests(TestCase):
+    def test_can_a_product_instance_be_created(self):
+        """Controls whether a product instance can be created."""
+        # given
+        product_name = "Elma"
+        # when
+        new_product = Product(name="Elma", category="meyve",
+                              origin="Amasya", price=8.75, count_in_stock=14)
+        # then
+        self.assertEqual(new_product.name, product_name)
+    def test_can_a_product_be_saved_to_database(self):
+        """Controls whether a product can be saved to database."""
+        # given
+        product_name = "Elma"
+        new_product = Product(name="Elma", category="meyve",
+                              origin="Amasya", price=8.75, count_in_stock=14)
+        new_product.save()
+        # when
+        new_product_from_db = Product.objects.filter(name=product_name)
+        # then
+        self.assertEqual(new_product_from_db[0].name, product_name)
